2fb06f26b4196ab83215e280bf21afb4ceffe9f0
yaotthaha <yaotthaha@foxmail.com>
2023-04-13 16:09:25 +0800

    Add URLTest Fallback Support

--- option/group.go
+++ option/group.go
@@ -7,10 +7,16 @@ type SelectorOutboundOptions struct {
 }
 
 type URLTestOutboundOptions struct {
-	Outbounds                 []string `json:"outbounds"`
-	URL                       string   `json:"url,omitempty"`
-	Interval                  Duration `json:"interval,omitempty"`
-	Tolerance                 uint16   `json:"tolerance,omitempty"`
-	IdleTimeout               Duration `json:"idle_timeout,omitempty"`
-	InterruptExistConnections bool     `json:"interrupt_exist_connections,omitempty"`
+	Outbounds                 []string               `json:"outbounds"`
+	URL                       string                 `json:"url,omitempty"`
+	Interval                  Duration               `json:"interval,omitempty"`
+	Tolerance                 uint16                 `json:"tolerance,omitempty"`
+	IdleTimeout               Duration               `json:"idle_timeout,omitempty"`
+	InterruptExistConnections bool                   `json:"interrupt_exist_connections,omitempty"`
+	Fallback                  URLTestFallbackOptions `json:"fallback,omitempty"`
+}
+
+type URLTestFallbackOptions struct {
+	Enabled  bool     `json:"enabled,omitempty"`
+	MaxDelay Duration `json:"max_delay,omitempty"`
 }
