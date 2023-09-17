8bb66c06462e32090876cf85ea597832bb71c08b
dyhkwong <50692134+dyhkwong@users.noreply.github.com>
2023-09-17 21:49:21 +0800

    feature: TLS certificate pinning

--- common/tls/utls_client.go
+++ common/tls/utls_client.go
@@ -4,8 +4,10 @@ package tls
 
 import (
 	"context"
+	"crypto/sha256"
 	"crypto/tls"
 	"crypto/x509"
+	"encoding/hex"
 	"math/rand"
 	"net"
 	"os"
@@ -196,6 +198,20 @@ func NewUTLSClient(ctx context.Context, serverAddress string, options option.Out
 			return nil, E.New("failed to parse certificate:\n\n", certificate)
 		}
 		tlsConfig.RootCAs = certPool
+	}
+	if options.CertificatePinSHA256 != "" {
+		tlsConfig.VerifyPeerCertificate = func(rawCerts [][]byte, verifiedChains [][]*x509.Certificate) error {
+			for _, rawCert := range rawCerts {
+				cert, err := x509.ParseCertificate(rawCert)
+				if err == nil {
+					hash := sha256.Sum256(cert.Raw)
+					if strings.ToLower(options.CertificatePinSHA256) == hex.EncodeToString(hash[:]) {
+						return nil
+					}
+				}
+			}
+			return E.New("certificate fingerprint mismatch")
+		}
 	}
 	id, err := uTLSClientHelloID(options.UTLS.Fingerprint)
 	if err != nil {
