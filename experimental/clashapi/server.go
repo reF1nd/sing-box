a75fcce51b35825cce94101123024528c6744d49
yaotthaha <yaotthaha@foxmail.com>
2023-09-28 11:03:09 +0800

    Add Clash Dashboard Build-in Support

--- experimental/clashapi/server.go
+++ experimental/clashapi/server.go
@@ -54,6 +54,7 @@ type Server struct {
 	externalUI               string
 	externalUIDownloadURL    string
 	externalUIDownloadDetour string
+	externalUIBuildin        bool
 }
 
 func NewServer(ctx context.Context, router adapter.Router, logFactory log.ObservableFactory, options option.ClashAPIOptions) (adapter.ClashServer, error) {
@@ -72,6 +73,7 @@ func NewServer(ctx context.Context, router adapter.Router, logFactory log.Observ
 		externalController:       options.ExternalController != "",
 		externalUIDownloadURL:    options.ExternalUIDownloadURL,
 		externalUIDownloadDetour: options.ExternalUIDownloadDetour,
+		externalUIBuildin:        options.ExternalUIBuildin,
 	}
 	server.urlTestHistory = service.PtrFromContext[urltest.HistoryStorage](ctx)
 	if server.urlTestHistory == nil {
@@ -130,6 +132,12 @@ func NewServer(ctx context.Context, router adapter.Router, logFactory log.Observ
 				fs.ServeHTTP(w, r)
 			})
 		})
+	} else if options.ExternalUIBuildin {
+		f, err := initDashboard()
+		if err != nil {
+			return nil, err
+		}
+		chiRouter.Group(f)
 	}
 	return server, nil
 }
@@ -149,7 +157,9 @@ func (s *Server) PreStart() error {
 
 func (s *Server) Start() error {
 	if s.externalController {
-		s.checkAndDownloadExternalUI()
+		if !s.externalUIBuildin {
+			s.checkAndDownloadExternalUI()
+		}
 		var (
 			listener net.Listener
 			err      error
