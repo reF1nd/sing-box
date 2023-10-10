ef2014a7db461027c1c6d5a4a6a0f852331c597c
yaotthaha <yaotthaha@foxmail.com>
2023-10-10 10:40:10 +0800

    Add reload support
    (cherry picked from commit https://github.com/rnetx/sing-box/commit/e98000c371095c1d5f7fcec4a02ffaaf22a68e47)

--- box.go
+++ box.go
@@ -51,6 +51,7 @@ type Box struct {
 	connection   *route.ConnectionManager
 	router       *route.Router
 	services     []adapter.LifecycleService
+	reloadChan   chan struct{}
 	done         chan struct{}
 }
 
@@ -97,6 +98,7 @@ func Context(
 
 func New(options Options) (*Box, error) {
 	createdAt := time.Now()
+	reloadChan := make(chan struct{}, 1)
 	ctx := options.Context
 	if ctx == nil {
 		ctx = context.Background()
@@ -189,7 +191,7 @@ func New(options Options) (*Box, error) {
 	service.MustRegister[adapter.NetworkManager](ctx, networkManager)
 	connectionManager := route.NewConnectionManager(logFactory.NewLogger("connection"))
 	service.MustRegister[adapter.ConnectionManager](ctx, connectionManager)
-	router := route.NewRouter(ctx, logFactory, routeOptions, dnsOptions)
+	router := route.NewRouter(ctx, logFactory, routeOptions, dnsOptions, reloadChan)
 	service.MustRegister[adapter.Router](ctx, router)
 	err = router.Initialize(routeOptions.Rules, routeOptions.RuleSet)
 	if err != nil {
@@ -396,6 +398,7 @@ func New(options Options) (*Box, error) {
 		logFactory:   logFactory,
 		logger:       logFactory.Logger(),
 		services:     services,
+		reloadChan:   reloadChan,
 		done:         make(chan struct{}),
 	}, nil
 }
@@ -532,4 +535,8 @@ func (s *Box) Inbound() adapter.InboundManager {
 
 func (s *Box) Outbound() adapter.OutboundManager {
 	return s.outbound
+}
+
+func (s *Box) ReloadChan() <-chan struct{} {
+	return s.reloadChan
 }
