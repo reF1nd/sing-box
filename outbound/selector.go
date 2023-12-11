7d831ec9980a21e1ef2ef6810ba87875a90c27ed
reF1nd <ref1nd@thunder-cloud.cc>
2023-12-11 22:44:49 +0800

    Add `IPIfNonMatch` and `IPOnDemand`

--- outbound/selector.go
+++ outbound/selector.go
@@ -258,6 +258,10 @@ func (s *Selector) Now() string {
 	return s.selected.Tag()
 }
 
+func (s *Selector) SelectedOutbound(network string) adapter.Outbound {
+	return s.selected
+}
+
 func (s *Selector) All() []string {
 	var all []string
 	for _, outbound := range s.outbounds {
@@ -306,4 +310,12 @@ func RealTag(detour adapter.Outbound) string {
 		return group.Now()
 	}
 	return detour.Tag()
+}
+
+func RealOutboundTag(detour adapter.Outbound, network string) string {
+	group, isGroup := detour.(adapter.OutboundGroup)
+	if !isGroup {
+		return detour.Tag()
+	}
+	return RealOutboundTag(group.SelectedOutbound(network), network)
 }
