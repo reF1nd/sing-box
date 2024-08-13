97ef8b3567f23f9b1b1ef9071f8e85089cab7a97
PuerNya <safarier@outlook.com>
2024-08-14 01:03:31 +0800

    add outbound provider to provide hot-update ability

--- option/config.go
+++ option/config.go
@@ -7,16 +7,17 @@ import (
 )
 
 type _Options struct {
-	RawMessage   json.RawMessage      `json:"-"`
-	Schema       string               `json:"$schema,omitempty"`
-	Log          *LogOptions          `json:"log,omitempty"`
-	DNS          *DNSOptions          `json:"dns,omitempty"`
-	NTP          *NTPOptions          `json:"ntp,omitempty"`
-	Inbounds     []Inbound            `json:"inbounds,omitempty"`
-	Outbounds    []Outbound           `json:"outbounds,omitempty"`
-	Route        *RouteOptions        `json:"route,omitempty"`
-	Experimental *ExperimentalOptions `json:"experimental,omitempty"`
-	Scripts      []ScriptOptions      `json:"scripts,omitempty"`
+	RawMessage        json.RawMessage      `json:"-"`
+	Schema            string               `json:"$schema,omitempty"`
+	Log               *LogOptions          `json:"log,omitempty"`
+	DNS               *DNSOptions          `json:"dns,omitempty"`
+	NTP               *NTPOptions          `json:"ntp,omitempty"`
+	Inbounds          []Inbound            `json:"inbounds,omitempty"`
+	Outbounds         []Outbound           `json:"outbounds,omitempty"`
+	Route             *RouteOptions        `json:"route,omitempty"`
+	OutboundProviders []OutboundProvider   `json:"outbound_providers,omitempty"`
+	Experimental      *ExperimentalOptions `json:"experimental,omitempty"`
+	Scripts           []ScriptOptions      `json:"scripts,omitempty"`
 }
 
 type Options _Options
@@ -38,4 +39,19 @@ type LogOptions struct {
 	Output       string `json:"output,omitempty"`
 	Timestamp    bool   `json:"timestamp,omitempty"`
 	DisableColor bool   `json:"-"`
+}
+
+type _OutboundProviderOptions struct {
+	Outbounds []Outbound `json:"outbounds"`
+}
+
+type OutboundProviderOptions _OutboundProviderOptions
+
+func (o *OutboundProviderOptions) UnmarshalJSON(content []byte) error {
+	decoder := json.NewDecoder(bytes.NewReader(content))
+	err := decoder.Decode((*_OutboundProviderOptions)(o))
+	if err != nil {
+		return err
+	}
+	return nil
 }
