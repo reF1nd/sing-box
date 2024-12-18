ffe065382b0bca8c036f8d8d4f34e2e4c37387fb
lux5am <lux.5am@yandex.com>
2024-12-18 11:31:48 +0800

    add outbound type pass
    Signed-off-by: lux5am <lux.5am@yandex.com>

--- constant/proxy.go
+++ constant/proxy.go
@@ -6,6 +6,7 @@ const (
 	TypeTProxy       = "tproxy"
 	TypeDirect       = "direct"
 	TypeBlock        = "block"
+	TypePass         = "pass"
 	TypeDNS          = "dns"
 	TypeSOCKS        = "socks"
 	TypeHTTP         = "http"
@@ -51,6 +52,8 @@ func ProxyDisplayName(proxyType string) string {
 		return "Direct"
 	case TypeBlock:
 		return "Block"
+	case TypePass:
+		return "Pass"
 	case TypeDNS:
 		return "DNS"
 	case TypeSOCKS:
