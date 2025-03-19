841e71498b02b7c14c7ffc9f62f8c444054b0bbc
reF1nd <ref1nd@thunder-cloud.cc>
2025-03-19 17:42:39 +0800

    Restore proxy protocol

--- option/inbound.go
+++ option/inbound.go
@@ -59,24 +59,21 @@ type InboundOptions struct {
 }
 
 type ListenOptions struct {
-	Listen               *badoption.Addr    `json:"listen,omitempty"`
-	ListenPort           uint16             `json:"listen_port,omitempty"`
-	BindInterface        string             `json:"bind_interface,omitempty"`
-	RoutingMark          FwMark             `json:"routing_mark,omitempty"`
-	ReuseAddr            bool               `json:"reuse_addr,omitempty"`
-	NetNs                string             `json:"netns,omitempty"`
-	TCPKeepAlive         badoption.Duration `json:"tcp_keep_alive,omitempty"`
-	TCPKeepAliveInterval badoption.Duration `json:"tcp_keep_alive_interval,omitempty"`
-	TCPFastOpen          bool               `json:"tcp_fast_open,omitempty"`
-	TCPMultiPath         bool               `json:"tcp_multi_path,omitempty"`
-	UDPFragment          *bool              `json:"udp_fragment,omitempty"`
-	UDPFragmentDefault   bool               `json:"-"`
-	UDPTimeout           UDPTimeoutCompat   `json:"udp_timeout,omitempty"`
-
-	// Deprecated: removed
-	ProxyProtocol bool `json:"proxy_protocol,omitempty"`
-	// Deprecated: removed
-	ProxyProtocolAcceptNoHeader bool `json:"proxy_protocol_accept_no_header,omitempty"`
+	Listen                      *badoption.Addr    `json:"listen,omitempty"`
+	ListenPort                  uint16             `json:"listen_port,omitempty"`
+	BindInterface               string             `json:"bind_interface,omitempty"`
+	RoutingMark                 FwMark             `json:"routing_mark,omitempty"`
+	ReuseAddr                   bool               `json:"reuse_addr,omitempty"`
+	NetNs                       string             `json:"netns,omitempty"`
+	TCPKeepAlive                badoption.Duration `json:"tcp_keep_alive,omitempty"`
+	TCPKeepAliveInterval        badoption.Duration `json:"tcp_keep_alive_interval,omitempty"`
+	TCPFastOpen                 bool               `json:"tcp_fast_open,omitempty"`
+	TCPMultiPath                bool               `json:"tcp_multi_path,omitempty"`
+	UDPFragment                 *bool              `json:"udp_fragment,omitempty"`
+	UDPFragmentDefault          bool               `json:"-"`
+	UDPTimeout                  UDPTimeoutCompat   `json:"udp_timeout,omitempty"`
+	ProxyProtocol               bool               `json:"proxy_protocol,omitempty"`
+	ProxyProtocolAcceptNoHeader bool               `json:"proxy_protocol_accept_no_header,omitempty"`
 	InboundOptions
 }
 
