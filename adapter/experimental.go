d0b40b8885b7f302903095c1d77100152d1ab31a
reF1nd <ref1nd@thunder-cloud.cc>
2025-04-18 17:19:48 +0800

    Add `path` option for remote provider

--- adapter/experimental.go
+++ adapter/experimental.go
@@ -7,6 +7,7 @@ import (
 	"io"
 	"time"
 
+	"github.com/sagernet/sing-box/common/hash"
 	"github.com/sagernet/sing/common/observable"
 	"github.com/sagernet/sing/common/varbin"
 )
@@ -68,6 +69,7 @@ type CacheFile interface {
 }
 
 type SavedBinary struct {
+	Hash        hash.HashType
 	Content     []byte
 	LastUpdated time.Time
 	LastEtag    string
@@ -76,6 +78,18 @@ type SavedBinary struct {
 func (s *SavedBinary) MarshalBinary() ([]byte, error) {
 	var buffer bytes.Buffer
 	err := binary.Write(&buffer, binary.BigEndian, uint8(1))
+	if err != nil {
+		return nil, err
+	}
+	hash, err := s.Hash.MarshalBinary()
+	if err != nil {
+		return nil, err
+	}
+	_, err = varbin.WriteUvarint(&buffer, uint64(len(hash)))
+	if err != nil {
+		return nil, err
+	}
+	_, err = buffer.Write(hash)
 	if err != nil {
 		return nil, err
 	}
@@ -106,6 +120,19 @@ func (s *SavedBinary) UnmarshalBinary(data []byte) error {
 	reader := bytes.NewReader(data)
 	var version uint8
 	err := binary.Read(reader, binary.BigEndian, &version)
+	if err != nil {
+		return err
+	}
+	hashLength, err := binary.ReadUvarint(reader)
+	if err != nil {
+		return err
+	}
+	hash := make([]byte, hashLength)
+	_, err = io.ReadFull(reader, hash)
+	if err != nil {
+		return err
+	}
+	err = s.Hash.UnmarshalBinary(hash)
 	if err != nil {
 		return err
 	}
