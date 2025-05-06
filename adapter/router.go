ca955bb8cb34fbdd4a1701fd2bb0a8fd495f434f
reF1nd <ref1nd@thunder-cloud.cc>
2025-05-07 00:26:57 +0800

    Add `domain_match_strategy` and `default_domain_match_strategy`

--- adapter/router.go
+++ adapter/router.go
@@ -30,6 +30,7 @@ type Router interface {
 	NeighborResolver() NeighborResolver
 	AppendTracker(tracker ConnectionTracker)
 	ResetNetwork()
+	DefaultDomainMatchStrategy() C.DomainMatchStrategy
 }
 
 type ConnectionTracker interface {
