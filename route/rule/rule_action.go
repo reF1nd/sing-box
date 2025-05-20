357922284a70f727637bc46185ea78b99832fb7f
reF1nd <ref1nd@thunder-cloud.cc>
2025-05-21 02:03:09 +0800

    Add `rcode` option for DNS reject action

--- route/rule/rule_action.go
+++ route/rule/rule_action.go
@@ -126,8 +126,9 @@ func NewDNSRuleAction(logger logger.ContextLogger, action option.DNSRuleAction) 
 		}
 	case C.RuleActionTypeReject:
 		return &RuleActionReject{
-			Method: action.RejectOptions.Method,
-			NoDrop: action.RejectOptions.NoDrop,
+			Rcode:  action.DNSRejectOptions.Rcode.Build(),
+			Method: action.DNSRejectOptions.Method,
+			NoDrop: action.DNSRejectOptions.NoDrop,
 			logger: logger,
 		}
 	case C.RuleActionTypePredefined:
@@ -285,6 +286,7 @@ func (r *RuleActionDirect) String() string {
 }
 
 type RuleActionReject struct {
+	Rcode       int
 	Method      string
 	NoDrop      bool
 	logger      logger.ContextLogger
