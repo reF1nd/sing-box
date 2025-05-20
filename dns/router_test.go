f5698723d94e06236b5d4b4babdf858839c6d1ac
reF1nd <ref1nd@thunder-cloud.cc>
2025-05-21 02:03:09 +0800

    Add `rcode` option for DNS reject action

--- dns/router_test.go
+++ dns/router_test.go
@@ -2411,7 +2411,7 @@ func TestLookupLegacyDNSModeDisabledReturnsRejectedErrorForRejectAction(t *testi
 				},
 				DNSRuleAction: option.DNSRuleAction{
 					Action: C.RuleActionTypeReject,
-					RejectOptions: option.RejectActionOptions{
+					DNSRejectOptions: option.DNSRejectActionOptions{
 						Method: C.RuleActionRejectMethodDefault,
 					},
 				},
@@ -2444,7 +2444,7 @@ func TestExchangeLegacyDNSModeDisabledReturnsRefusedResponseForRejectAction(t *t
 				},
 				DNSRuleAction: option.DNSRuleAction{
 					Action: C.RuleActionTypeReject,
-					RejectOptions: option.RejectActionOptions{
+					DNSRejectOptions: option.DNSRejectActionOptions{
 						Method: C.RuleActionRejectMethodDefault,
 					},
 				},
@@ -2479,7 +2479,7 @@ func TestExchangeLegacyDNSModeDisabledReturnsDropErrorForRejectDropAction(t *tes
 				},
 				DNSRuleAction: option.DNSRuleAction{
 					Action: C.RuleActionTypeReject,
-					RejectOptions: option.RejectActionOptions{
+					DNSRejectOptions: option.DNSRejectActionOptions{
 						Method: C.RuleActionRejectMethodDrop,
 					},
 				},
