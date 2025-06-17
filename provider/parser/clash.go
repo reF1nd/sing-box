1f8ee4b4154431202035d5432f3bc424d6330add
reF1nd <ref1nd@thunder-cloud.cc>
2025-06-17 22:46:08 +0800

    Add tls-pin-sha256 support to outbound provider

--- provider/parser/clash.go
+++ provider/parser/clash.go
@@ -215,6 +215,7 @@ type TLSOption struct {
 	SNI               string          `yaml:"sni,omitempty"`
 	ServerName        string          `yaml:"server-name,omitempty"`
 	SkipCertVerify    bool            `yaml:"skip-cert-verify,omitempty"`
+	Fingerprint       string          `yaml:"fingerprint,omitempty"`
 	ALPN              []string        `yaml:"alpn,omitempty"`
 	ClientFingerprint string          `yaml:"client-fingerprint,omitempty"`
 	CustomCA          string          `yaml:"ca,omitempty"`
@@ -235,15 +236,16 @@ func (t *TLSOption) Build() *option.OutboundTLSOptions {
 		customCAString = strings.Split(t.CustomCAString, "\n")
 	}
 	return &option.OutboundTLSOptions{
-		Enabled:         t.TLS,
-		ServerName:      t.SNI,
-		Insecure:        t.SkipCertVerify,
-		ALPN:            t.ALPN,
-		UTLS:            clashClientFingerprint(t.ClientFingerprint),
-		Certificate:     customCAString,
-		CertificatePath: t.CustomCA,
-		ECH:             t.ECHOpts.Build(),
-		Reality:         t.RealityOpts.Build(),
+		Enabled:              t.TLS,
+		ServerName:           t.SNI,
+		Insecure:             t.SkipCertVerify,
+		CertificatePinSHA256: t.Fingerprint,
+		ALPN:                 t.ALPN,
+		UTLS:                 clashClientFingerprint(t.ClientFingerprint),
+		Certificate:          customCAString,
+		CertificatePath:      t.CustomCA,
+		ECH:                  t.ECHOpts.Build(),
+		Reality:              t.RealityOpts.Build(),
 	}
 }
 
