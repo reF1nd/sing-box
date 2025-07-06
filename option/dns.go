385e80c9780cb62a37f50febaec381f78d6b76c8
安容 <HystericalDragons@proton.me>
2025-07-06 15:41:13 +0800

    DNS: Add `reuse` option for TCP

--- option/dns.go
+++ option/dns.go
@@ -203,6 +203,11 @@ type RemoteDNSServerOptions struct {
 	DNSServerAddressOptions
 }
 
+type RemoteTCPDNSServerOptions struct {
+	RemoteDNSServerOptions
+	Reuse bool `json:"reuse,omitempty"`
+}
+
 type RemoteTLSDNSServerOptions struct {
 	RemoteDNSServerOptions
 	OutboundTLSOptionsContainer
