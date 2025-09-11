490ac36b4c2ad653bdee36c35723cea7488f05d7
reF1nd <ref1nd@thunder-cloud.cc>
2025-09-11 18:25:08 +0800

    Add kTLS support for outbound provider

--- provider/parser/clash.go
+++ provider/parser/clash.go
@@ -699,6 +699,8 @@ type TLSOptions struct {
 	PrivateKey        string          `yaml:"private-key,omitempty"`
 	ECHOpts           *ECHOptions     `yaml:"ech-opts,omitempty"`
 	RealityOpts       *RealityOptions `yaml:"reality-opts,omitempty"`
+	KernelTx          bool            `yaml:"kernel-tx,omitempty"`
+	KernelRx          bool            `yaml:"kernel-rx,omitempty"`
 }
 
 func (t *TLSOptions) Build() *option.OutboundTLSOptions {
@@ -715,6 +717,8 @@ func (t *TLSOptions) Build() *option.OutboundTLSOptions {
 		CertificatePath: t.CustomCA,
 		ECH:             t.ECHOpts.Build(),
 		Reality:         t.RealityOpts.Build(),
+		KernelTx:        t.KernelTx,
+		KernelRx:        t.KernelRx,
 	}
 	if strings.HasPrefix(t.Certificate, "-----BEGIN ") {
 		options.ClientCertificate = trimStringArray(strings.Split(t.Certificate, "\n"))
