25c3881d5e03bca3259526589f63448051a72bc1
reF1nd <ref1nd@thunder-cloud.cc>
2026-02-06 22:01:40 +0800

    Add configurable `gso` option for WireGuard and Tailscale endpoint

--- protocol/tailscale/endpoint.go
+++ protocol/tailscale/endpoint.go
@@ -120,6 +120,7 @@ type Endpoint struct {
 
 	systemInterface     bool
 	systemInterfaceName string
+	systemInterfaceGSO  bool
 	systemInterfaceMTU  uint32
 	keyAuth             bool
 	serverStarted       bool
@@ -168,6 +169,10 @@ func NewEndpoint(ctx context.Context, router adapter.Router, logger log.ContextL
 		udpTimeout = time.Duration(options.UDPTimeout)
 	} else {
 		udpTimeout = C.UDPTimeout
+	}
+	gso := options.SystemInterface
+	if options.SystemInterfaceGSO != nil {
+		gso = *options.SystemInterfaceGSO
 	}
 	outboundDialer, err := dialer.NewWithOptions(dialer.Options{
 		Context:          ctx,
@@ -233,6 +238,7 @@ func NewEndpoint(ctx context.Context, router adapter.Router, logger log.ContextL
 		udpTimeout:                 udpTimeout,
 		icmpTimeout:                C.ICMPTimeout,
 		systemInterface:            options.SystemInterface,
+		systemInterfaceGSO:         gso,
 		systemInterfaceName:        options.SystemInterfaceName,
 		systemInterfaceMTU:         options.SystemInterfaceMTU,
 		keyAuth:                    options.AuthKey != "",
@@ -294,7 +300,7 @@ func (t *Endpoint) start() error {
 		tunOptions := tun.Options{
 			Name:                      tunName,
 			MTU:                       mtu,
-			GSO:                       true,
+			GSO:                       t.systemInterfaceGSO,
 			InterfaceScope:            true,
 			InterfaceMonitor:          t.network.InterfaceMonitor(),
 			InterfaceFinder:           t.network.InterfaceFinder(),
