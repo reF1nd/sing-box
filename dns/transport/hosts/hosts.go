095b85f82108781c7cef0d586e65d465fca73706
reF1nd <ref1nd@thunder-cloud.cc>
2026-05-06 02:50:34 +0800

    Add domain alias support for DNS hosts server

--- dns/transport/hosts/hosts.go
+++ dns/transport/hosts/hosts.go
@@ -10,6 +10,7 @@ import (
 	"github.com/sagernet/sing-box/dns"
 	"github.com/sagernet/sing-box/log"
 	"github.com/sagernet/sing-box/option"
+	"github.com/sagernet/sing/service"
 	"github.com/sagernet/sing/service/filemanager"
 
 	mDNS "github.com/miekg/dns"
@@ -26,14 +27,17 @@ var (
 
 type Transport struct {
 	dns.TransportAdapter
-	files      []*File
-	predefined map[string][]netip.Addr
+	ctx              context.Context
+	files            []*File
+	predefined       map[string][]netip.Addr
+	predefinedDomain map[string]string
 }
 
 func NewTransport(ctx context.Context, logger log.ContextLogger, tag string, options option.HostsDNSServerOptions) (adapter.DNSTransport, error) {
 	var (
-		files      []*File
-		predefined = make(map[string][]netip.Addr)
+		files            []*File
+		predefined       = make(map[string][]netip.Addr)
+		predefinedDomain = make(map[string]string)
 	)
 	if len(options.Path) == 0 {
 		defaultFile, err := NewDefault()
@@ -48,13 +52,20 @@ func NewTransport(ctx context.Context, logger log.ContextLogger, tag string, opt
 	}
 	if options.Predefined != nil {
 		for _, entry := range options.Predefined.Entries() {
-			predefined[mDNS.CanonicalName(entry.Key)] = entry.Value
+			key := mDNS.CanonicalName(entry.Key)
+			if entry.Value.Domain != "" {
+				predefinedDomain[key] = mDNS.CanonicalName(entry.Value.Domain)
+			} else {
+				predefined[key] = entry.Value.Addresses
+			}
 		}
 	}
 	return &Transport{
 		TransportAdapter: dns.NewTransportAdapter(C.DNSTypeHosts, tag, nil),
+		ctx:              ctx,
 		files:            files,
 		predefined:       predefined,
+		predefinedDomain: predefinedDomain,
 	}, nil
 }
 
@@ -73,6 +84,9 @@ func (t *Transport) PreferredDomain(domain string) bool {
 	if _, loaded := t.predefined[domain]; loaded {
 		return true
 	}
+	if _, loaded := t.predefinedDomain[domain]; loaded {
+		return true
+	}
 	for _, file := range t.files {
 		if len(file.Lookup(domain)) > 0 {
 			return true
@@ -87,6 +101,70 @@ func (t *Transport) Exchange(ctx context.Context, message *mDNS.Msg) (*mDNS.Msg,
 	if question.Qtype == mDNS.TypeA || question.Qtype == mDNS.TypeAAAA {
 		if addresses, ok := t.predefined[domain]; ok {
 			return dns.FixedResponse(message.Id, question, addresses, C.DefaultDNSTTL), nil
+		}
+		if targetDomain, ok := t.predefinedDomain[domain]; ok {
+			dnsRouter := service.FromContext[adapter.DNSRouter](t.ctx)
+			targetMsg := &mDNS.Msg{
+				MsgHdr: mDNS.MsgHdr{
+					RecursionDesired: true,
+				},
+				Question: []mDNS.Question{{
+					Name:   targetDomain,
+					Qtype:  question.Qtype,
+					Qclass: mDNS.ClassINET,
+				}},
+			}
+			resolveCtx, loopDetected := dns.ContextWithAliasResolution(adapter.OverrideContext(ctx), domain, targetDomain)
+			if loopDetected {
+				return &mDNS.Msg{
+					MsgHdr: mDNS.MsgHdr{
+						Id:       message.Id,
+						Rcode:    mDNS.RcodeServerFailure,
+						Response: true,
+					},
+					Question: []mDNS.Question{question},
+				}, nil
+			}
+			response, err := dnsRouter.Exchange(resolveCtx, targetMsg, adapter.DNSQueryOptions{DisableOptimisticCache: true})
+			if err != nil {
+				return nil, err
+			}
+			if response.Rcode != mDNS.RcodeSuccess {
+				return &mDNS.Msg{
+					MsgHdr: mDNS.MsgHdr{
+						Id:       message.Id,
+						Rcode:    response.Rcode,
+						Response: true,
+					},
+					Question: []mDNS.Question{question},
+				}, nil
+			}
+			if len(response.Answer) == 0 {
+				return &mDNS.Msg{
+					MsgHdr: mDNS.MsgHdr{
+						Id:       message.Id,
+						Rcode:    mDNS.RcodeSuccess,
+						Response: true,
+					},
+					Question: []mDNS.Question{question},
+					Ns:       response.Ns,
+					Extra:    response.Extra,
+				}, nil
+			}
+			var ttl uint32 = response.Answer[0].Header().Ttl
+			var addresses []netip.Addr
+			for _, rr := range response.Answer {
+				if rr.Header().Ttl < ttl {
+					ttl = rr.Header().Ttl
+				}
+				switch record := rr.(type) {
+				case *mDNS.A:
+					addresses = append(addresses, netip.AddrFrom4([4]byte(record.A.To4())))
+				case *mDNS.AAAA:
+					addresses = append(addresses, netip.AddrFrom16([16]byte(record.AAAA)))
+				}
+			}
+			return dns.FixedResponse(message.Id, question, addresses, ttl), nil
 		}
 		for _, file := range t.files {
 			addresses := file.Lookup(domain)
