ca5069d77b3eb6e12d4c90a596be59b9950fcbbc
reF1nd <ref1nd@thunder-cloud.cc>
2026-07-19 00:53:06 +0800

    Resolve L3 domains via selected outbound

--- protocol/tailscale/endpoint.go
+++ protocol/tailscale/endpoint.go
@@ -65,6 +65,7 @@ import (
 
 var (
 	_ adapter.OutboundWithPreferredRoutes = (*Endpoint)(nil)
+	_ adapter.FlowOutboundDomainResolver  = (*Endpoint)(nil)
 	_ adapter.InterfaceUpdateListener     = (*Endpoint)(nil)
 	_ dialer.PacketDialerWithDestination  = (*Endpoint)(nil)
 	_ tun.Port                            = (*Endpoint)(nil)
