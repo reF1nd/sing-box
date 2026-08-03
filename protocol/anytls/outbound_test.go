019debde12cc9052abd96c451934a182486b89c2
reF1nd <ref1nd@thunder-cloud.cc>
2026-08-03 23:36:45 +0800

    Respect `disable_reuse` in AnyTLS URLTest

--- protocol/anytls/outbound_test.go
+++ protocol/anytls/outbound_test.go
@@ -7,6 +7,7 @@ import (
 	"github.com/sagernet/sing-box/adapter"
 	C "github.com/sagernet/sing-box/constant"
 	"github.com/sagernet/sing-box/option"
+	"github.com/sagernet/sing/common"
 	"github.com/sagernet/sing/common/json"
 	"github.com/sagernet/sing/common/logger"
 
@@ -14,16 +15,22 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func TestOutboundTCPFastOpen(t *testing.T) {
-	outbound, err := NewOutbound(context.Background(), nil, logger.NOP(), "test", option.AnyTLSOutboundOptions{
-		DialerOptions:               option.DialerOptions{AbstractDialerOptions: option.AbstractDialerOptions{TCPFastOpen: true}},
-		ServerOptions:               option.ServerOptions{Server: "127.0.0.1", ServerPort: 443},
-		OutboundTLSOptionsContainer: option.OutboundTLSOptionsContainer{TLS: &option.OutboundTLSOptions{Enabled: true}},
-		Password:                    "password",
-	})
-	require.NoError(t, err)
-	require.NoError(t, outbound.(*Outbound).Start(adapter.StartStateInitialize))
-	require.NoError(t, outbound.(*Outbound).Close())
+func TestOutboundOptions(t *testing.T) {
+	for _, disableReuse := range []bool{false, true} {
+		created, err := NewOutbound(context.Background(), nil, logger.NOP(), "test", option.AnyTLSOutboundOptions{
+			DialerOptions:               option.DialerOptions{AbstractDialerOptions: option.AbstractDialerOptions{TCPFastOpen: true}},
+			ServerOptions:               option.ServerOptions{Server: "127.0.0.1", ServerPort: 443},
+			OutboundTLSOptionsContainer: option.OutboundTLSOptionsContainer{TLS: &option.OutboundTLSOptions{Enabled: true}},
+			Password:                    "password", DisableReuse: disableReuse, ClientMetadata: common.Ptr(""),
+		})
+		require.NoError(t, err)
+		outbound := created.(*Outbound)
+		require.Equal(t, disableReuse, outbound.clientOptions.DisableReuse)
+		require.Equal(t, !disableReuse, outbound.MultiplexEnabled())
+		require.Empty(t, outbound.clientOptions.ClientMetadata)
+		require.NoError(t, outbound.Start(adapter.StartStateInitialize))
+		require.NoError(t, outbound.Close())
+	}
 }
 
 func TestInterfaceUpdated(t *testing.T) {
@@ -45,4 +52,9 @@ func TestClientMetadataOrDefault(t *testing.T) {
 		require.NoError(t, json.Unmarshal(encoded, &roundTrip))
 		require.Equal(t, options.ClientMetadata, roundTrip.ClientMetadata)
 	}
+}
+
+func TestMultiplexEnabled(t *testing.T) {
+	require.True(t, (&Outbound{}).MultiplexEnabled())
+	require.False(t, (&Outbound{disableReuse: true}).MultiplexEnabled())
 }
